2c5c5e10c6bc1d726e091351a1493ebb0e8d15f1
Sebastian Safari <ssebs@ymail.com>
2021-01-05 16:24:35 -0800

    working inline replace

--- extension/background.js
+++ extension/background.js
@@ -3,13 +3,46 @@
 // TODO: Make this not a hack
 const BASE_URL = "http://heaviside.parc.xerox.com:5000";
 
+// To create a new tab...
+// chrome.contextMenus.create({
+//   title: "PunKtuate!",
+//   contexts: ["selection"],
+//   onclick: (info, tab) => {
+//     const selectedText = info.selectionText;
+//     const newUrl = BASE_URL + "/?q=" + encodeURIComponent(selectedText);
+//     // chrome.tabs.create({ url: newUrl });
+//   },
+// });
+
+// To replace inline
+function mycallback(info, tab) {
+  // console.log(tab);
+  if (!tab) {
+    return;
+  }
+  chrome.tabs.sendMessage(tab.id, { type: "getVal", value: null }, (val) => {
+    if (chrome.runtime.lastError) {
+      setTimeout(mycallback, 1000);
+    } else {
+      // console.log(`recv: ${val}`);
+
+      // TOOD: Post to api & get response
+      const newVal = "__" + val + "__";
+
+      // Send another message with new text
+      chrome.tabs.sendMessage(
+        tab.id,
+        { type: "setVal", value: newVal },
+        (resp) => {
+          console.log(resp);
+        }
+      );
+    }
+  });
+}
+
 chrome.contextMenus.create({
   title: "PunKtuate!",
-  contexts: ["selection"],
-  onclick: (info) => {
-    const selectedText = info.selectionText;
-    // window.alert(selectedText);
-    const newUrl = BASE_URL + "/?q=" + encodeURIComponent(selectedText);
-    chrome.tabs.create({ url: newUrl });
-  },
+  contexts: ["selection", "editable"],
+  onclick: mycallback,
 });
---
+++ extension/contentScript.js
@@ -0,0 +1,48 @@
+// contentScript.js
+let elem = null;
+let theText = null;
+
+document.addEventListener(
+  "contextmenu",
+  (e) => {
+    elem = document.activeElement;
+    let startPos = elem.selectionStart;
+    let endPos = elem.selectionEnd;
+    theText = elem.value.substring(startPos, endPos);
+
+    // console.log(theText);
+  },
+  true
+);
+
+function replaceSelectedText(elem, text) {
+  var txtArea = elem;
+  if (txtArea.selectionStart != undefined) {
+    var startPos = txtArea.selectionStart;
+    var endPos = txtArea.selectionEnd;
+
+    let newTxt =
+      txtArea.value.slice(0, startPos) + text + txtArea.value.slice(endPos);
+
+    // const selectedText = txtArea.value.substring(startPos, endPos);
+    // console.log(text);
+    // console.log(selectedText);
+    // console.log(newTxt);
+
+    txtArea.value = newTxt;
+  }
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type == "getVal") {
+    // console.log("getval");
+    sendResponse(theText);
+  }
+  if (request.type == "setVal") {
+    // console.log("setval");
+    // console.log(request.value);
+
+    replaceSelectedText(elem, request.value);
+  }
+  return true;
+});
