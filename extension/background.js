5dda97592131fe00dcc12ffe7e625038e159b9aa
Sebastian Safari <ssafari@parc.com>
2021-01-05 16:54:32 -0800

    format text

--- extension/background.js
+++ extension/background.js
@@ -16,59 +16,59 @@ const BASE_URL = "http://heaviside.parc.xerox.com:5000";
 
 // To replace inline
 function mycallback(info, tab) {
-  // console.log(tab);
-  if (!tab) {
-    return;
-  }
-  chrome.tabs.sendMessage(tab.id, { type: "getVal", value: null }, (val) => {
-    if (chrome.runtime.lastError) {
-      setTimeout(mycallback, 1000);
-    } else {
-      // console.log(`recv: ${val}`);
-
-      // TOOD: Post to api & get response
-      // const newVal = "__" + val + "__";
-      fetch(`${BASE_URL}/punctuate`, {
-        method: "post",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          input_text: val,
-          text_style: "written",
-        }),
-      })
-        .then((r) => r.json())
-        .then((resp) => {
-          console.log(resp);
-          // Send another message with new text
-          chrome.tabs.sendMessage(
-            tab.id,
-            { type: "setVal", value: resp.text },
-            (res) => {
-              console.log(res);
-            }
-          );
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-
-      // // Send another message with new text
-      // chrome.tabs.sendMessage(
-      //   tab.id,
-      //   { type: "setVal", value: newVal },
-      //   (resp) => {
-      //     console.log(resp);
-      //   }
-      // );
+    // console.log(tab);
+    if (!tab) {
+        return;
     }
-  });
+    chrome.tabs.sendMessage(tab.id, { type: "getVal", value: null }, (val) => {
+        if (chrome.runtime.lastError) {
+            setTimeout(mycallback, 1000);
+        } else {
+            // console.log(`recv: ${val}`);
+
+            // TOOD: Post to api & get response
+            // const newVal = "__" + val + "__";
+            fetch(`${BASE_URL}/punctuate`, {
+                method: "post",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    input_text: val,
+                    text_style: "written",
+                }),
+            })
+                .then((r) => r.json())
+                .then((resp) => {
+                    console.log(resp);
+                    // Send another message with new text
+                    chrome.tabs.sendMessage(
+                        tab.id,
+                        { type: "setVal", value: resp.text },
+                        (res) => {
+                            console.log(res);
+                        }
+                    );
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+
+            // // Send another message with new text
+            // chrome.tabs.sendMessage(
+            //   tab.id,
+            //   { type: "setVal", value: newVal },
+            //   (resp) => {
+            //     console.log(resp);
+            //   }
+            // );
+        }
+    });
 }
 
 chrome.contextMenus.create({
-  title: "PunKtuate!",
-  contexts: ["selection", "editable"],
-  onclick: mycallback,
+    title: "PunKtuate!",
+    contexts: ["selection", "editable"],
+    onclick: mycallback,
 });
--- extension/contentScript.js
+++ extension/contentScript.js
@@ -3,46 +3,48 @@ let elem = null;
 let theText = null;
 
 document.addEventListener(
-  "contextmenu",
-  (e) => {
-    elem = document.activeElement;
-    let startPos = elem.selectionStart;
-    let endPos = elem.selectionEnd;
-    theText = elem.value.substring(startPos, endPos);
+    "contextmenu",
+    (e) => {
+        elem = document.activeElement;
+        let startPos = elem.selectionStart;
+        let endPos = elem.selectionEnd;
+        theText = elem.value.substring(startPos, endPos);
 
-    // console.log(theText);
-  },
-  true
+        // console.log(theText);
+    },
+    true
 );
 
 function replaceSelectedText(elem, text) {
-  var txtArea = elem;
-  if (txtArea.selectionStart != undefined) {
-    var startPos = txtArea.selectionStart;
-    var endPos = txtArea.selectionEnd;
+    var txtArea = elem;
+    if (txtArea.selectionStart != undefined) {
+        var startPos = txtArea.selectionStart;
+        var endPos = txtArea.selectionEnd;
 
-    let newTxt =
-      txtArea.value.slice(0, startPos) + text + txtArea.value.slice(endPos);
+        let newTxt =
+            txtArea.value.slice(0, startPos) +
+            text +
+            txtArea.value.slice(endPos);
 
-    // const selectedText = txtArea.value.substring(startPos, endPos);
-    // console.log(text);
-    // console.log(selectedText);
-    // console.log(newTxt);
+        // const selectedText = txtArea.value.substring(startPos, endPos);
+        // console.log(text);
+        // console.log(selectedText);
+        // console.log(newTxt);
 
-    txtArea.value = newTxt;
-  }
+        txtArea.value = newTxt;
+    }
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.type == "getVal") {
-    // console.log("getval");
-    sendResponse(theText);
-  }
-  if (request.type == "setVal") {
-    // console.log("setval");
-    // console.log(request.value);
+    if (request.type == "getVal") {
+        // console.log("getval");
+        sendResponse(theText);
+    }
+    if (request.type == "setVal") {
+        // console.log("setval");
+        // console.log(request.value);
 
-    replaceSelectedText(elem, request.value);
-  }
-  return true;
+        replaceSelectedText(elem, request.value);
+    }
+    return true;
 });
